da1df1fa03308661179446f4929c4852c1bcbdf2
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-08 15:47:07 +0100

    Add files via upload

---
+++ Javadoc2/src/ArithmeticOperators.java
@@ -0,0 +1,64 @@
+/**
+ * The type Arithmetic operators.
+ */
+public class ArithmeticOperators {
+
+
+    /**
+     * Sum.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     */
+    public void sum(int num1, int num2) {
+
+        int sum1 = num1 + num2;
+
+        System.out.println(sum1);
+
+
+    }
+
+    /**
+     * Min.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     */
+    public void min(int num1, int num2) {
+
+        int min1 = num1 - num2;
+
+        System.out.println(min1);
+    }
+
+    /**
+     * Div.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     */
+    public void div(int num1, int num2) {
+
+        int div1 = num1 / num2;
+
+        System.out.println(div1);
+    }
+
+    /**
+     * Mol.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     */
+    public void mol(int num1, int num2) {
+
+        int mol1 = num1 * num2;
+
+        System.out.println(mol1);
+    }
+}
+
+
+
+
---
+++ Javadoc2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ArithmeticOperators calcoli = new ArithmeticOperators();
+
+        calcoli.sum( 1, 2);
+        calcoli.div( 1, 2);
+        calcoli.min( 2, 1);
+        calcoli.mol( 1, 2);
+
+        System.out.println(calcoli);
+
+    }
+}
+//Author: Priolo Christian
+
+// Short method to have some calculator in 1 step
